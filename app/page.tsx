b0951160a59adb879c32d7043bd6ba9ff5f56b06
pupunha-dev <pupunha.dev@gmail.com>
2024-07-15 15:02:50 -0300

    Config word wrap vscode

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <main>
       <h1> Portfólio Pupunha</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quasi hic necessitatibus dicta fugiat amet autem debitis harum voluptatem dolorem eos corporis ratione, aperiam quae a atque consequuntur commodi ipsam.</p>
     </main>
   );
 }
