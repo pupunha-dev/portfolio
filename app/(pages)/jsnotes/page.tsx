003358ca26bac47677997c1581155ee4c3d9ade6
pupunha-dev <pupunha.dev@gmail.com>
2024-07-18 17:26:07 -0300

    map implementation on shadcn components

--- app/(pages)/jsnotes/page.tsx
+++ app/(pages)/jsnotes/page.tsx
@@ -1,58 +1,51 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
+import {Accordion,AccordionContent,AccordionItem,AccordionTrigger} from "@/components/ui/accordion"
+import {Tooltip,TooltipContent,TooltipProvider,TooltipTrigger} from "@/components/ui/tooltip"
 
+
+const jsThemes = [
+  {
+    tag: 'item-1', title: 'Sintaxe Básica', content: [
+      {theme: 'Variáveis', subTheme:''},
+      {theme: 'Tipos de Dados',subTheme:''},
+      {theme: 'Operadores', subTheme:''}]},
+  // {tag: 'item-2', title: 'Estruturas de Controle', content: ['Condicionais','Loops']},
+  // {tag: 'item-3', title: 'Funções', content: ['Declarações','Funções anônimas','Arrow Functions','Parâmetros e Rest/Spread']},
+  // {tag: 'item-4', title: 'Arrays', content: ['Métodos Básicos','Métodos Avançados','Destructuring']},
+  // {tag: 'item-5', title: 'Objetos', content: ['Objetos','Propriedades e Métodos','Destructuring']},
+]
 
 export default function JsNotes() {
   return (
     <div className="flex flex-col text-[#fafafa] p-8 space-y-6">
 
+      <h1 className="text-2xl font-black
+      
+      ">Meu Roadmap</h1>
+
       <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+      {jsThemes.map((titulo, i)=>(
+        <AccordionItem value={titulo.tag} key={i}>
+          <AccordionTrigger>{titulo.title}</AccordionTrigger>
           <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
+            <ul>
+              {titulo.content.map((items, itendId)=>(
+                <li key={itendId}>
+                  {items} - 
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger className="text-xs text-green-500">Hover</TooltipTrigger>
+                      <TooltipContent>
+                        <p>{items}</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </li>
+              ))}
+            </ul>
           </AccordionContent>
         </AccordionItem>
+      ))}
       </Accordion>
-
-
-      <ul>
-        <li className="font-black text-xl">Sintaxe Básica</li>
-        <li>Variáveis</li>
-        <li>Tipos de dados (number, string, boolean, null, undefined)</li>
-        <li>Operadores (aritméticos, lógicos e relacionais)</li>
-      </ul>
-
-      <ul>
-        <li className="font-black text-xl">Estruturas de Controle</li>
-        <li>Condicionais (if, else, switch)</li>
-        <li>Loops (for, while, do-while)</li>
-      </ul>
-
-      <ul>
-        <li className="font-black text-xl">Funções</li>
-        <li>Declarações</li>
-        <li>Funções anônimas</li>
-        <li>Arrow Functions</li>
-        <li>Parâmetros e Rest/Spread</li>
-      </ul>
-
-      <ul>
-        <li className="font-black text-xl">Arrays</li>
-        <li>Métodos Básicos (pop, push, shift, unshift, slice)</li>
-        <li>Métodos Avançados (map, filter, reduce, find)</li>
-        <li>Destructuring</li>
-      </ul>
-
-      <ul>
-        <li className="font-black text-xl">Objetos</li>
-        <li>Propriedades e Métodos</li>
-        <li>Destructuring</li>
-      </ul>
     </div>
   )
 }
